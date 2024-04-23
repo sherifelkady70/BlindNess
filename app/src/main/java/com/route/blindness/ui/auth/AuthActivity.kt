c1049d21ff85c29943fb47f9c835497858b9f71b
sherif Abdelrhman <sellkady038@gmail.com>
2024-04-23 09:52:01 +0200

    Finish Design Login fragment.

---
+++ app/src/main/java/com/route/blindness/ui/auth/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.route.blindness.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.route.blindness.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+}
