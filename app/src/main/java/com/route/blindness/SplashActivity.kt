e2e4ce21e9ee285ebd677a0267fe2173b0ba0598
sherif Abdelrhman <sellkady038@gmail.com>
2024-04-23 08:30:31 +0200

    Finish Splash Activity.

---
+++ app/src/main/java/com/route/blindness/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.route.blindness
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        navigateToAuthActivity()
+    }
+
+
+    private fun navigateToAuthActivity(){
+        Handler(mainLooper).postDelayed({
+         startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+        },2000)
+    }
+}
